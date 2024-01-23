7f2546c87b96aecb30eca347cfc398cdcba7207d
Vini <126819626+vin1i@users.noreply.github.com>
2024-01-23 20:12:19 -0300

    Adição de comentário Barra de busca
    Comentando Lógica de barra de busca para melhorar depois...

--- src/components/searchBar/Search.jsx
+++ src/components/searchBar/Search.jsx
@@ -1,19 +1,31 @@
-import React, {useState} from 'react';
+import React from 'react';
 //import { gamesPc } from '../computer/Data';
 import './search.css';
 
-// eslint-disable-next-line react/prop-types
-function Search({ onSearch }) {
 
-  const [searchValue, setSearchValue] = useState('');
+
+/* ----------------Form com Lógica de Busca em Andamento-------------------*/
+function Search(/*{onSearch}*/) {
+
+  /* const [searchValue, setSearchValue] = useState('');
   
   const handleSubmit = (e) => {
     e.preventDefault();
     onSearch(searchValue);
-  };
+  };*/
 
   return ( 
-
+    <form className="search__bar" >
+      <input 
+        type="search"
+        placeholder="Buscar jogos"
+        className="search__input"
+        required />
+     
+      
+      <button type="submit" className="search__button"> 
+        <i className="bx bx-search-alt search__icon"></i></button> </form>
+  /* --------------Form com Lógica de Busca em Andamento-----------------
     <form className="search__bar" onSubmit={handleSubmit}>
       <input 
         type="search"
@@ -25,7 +37,7 @@ function Search({ onSearch }) {
        
       { searchValue }
       <button type="submit" className="search__button"> 
-        <i className="bx bx-search-alt search__icon"></i></button> </form>
+        <i className="bx bx-search-alt search__icon"></i></button> </form>*/
   );
 }
 
